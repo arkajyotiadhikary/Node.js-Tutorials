2ccf75d0ab5c4b4aa71e0ec5c80eadec8140de86
Arka <arkajyotiadhikary15@gmail.com>
2020-10-01 01:54:06 +0530

    url

---
+++ W3School/6.Node.js URL Module/fileserver.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+
+const server = http.createServer((req,res)=>
+{
+    const q = url.parse(req.url,true);
+    const filename ="." + q.pathname;
+
+    fs.readFile(filename,(err,data)=>
+    {
+        if(err)
+        {
+            res.writeHead(404,{'Content_Type':'text/html'});
+            return res.end('404 not found');
+        }
+        else
+        {
+            res.writeHead(200,{'Content_Type':'text/html'});
+            res.write(data);
+            res.end();
+        }
+    });
+});
+
+server.listen(8080);
